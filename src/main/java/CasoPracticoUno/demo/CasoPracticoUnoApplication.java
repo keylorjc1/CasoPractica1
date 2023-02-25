5391c9be0975f210e542e25a9c3d0ef006d4a7de
keylorjc1 <keylorjc1@hotmail.com>
2023-02-25 10:14:52 -0600

    Created casoPractico

---
+++ src/main/java/CasoPracticoUno/demo/CasoPracticoUnoApplication.java
@@ -0,0 +1,13 @@
+package CasoPracticoUno.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasoPracticoUnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasoPracticoUnoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CasoPracticoUno/demo/CasoPracticoUnoApplicationTests.java
@@ -0,0 +1,13 @@
+package CasoPracticoUno.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasoPracticoUnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
